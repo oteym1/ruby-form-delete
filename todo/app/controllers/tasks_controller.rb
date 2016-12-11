5a963f0f1257e8fd5c2cbdc8b804f2bc75c0a233
Malakye Otey <oteym1@tcnj.edu>
2016-12-11 08:02:42 -0500

    rails/todo test
    did it lovely following a tutorial
    can edit and delete

---
+++ todo/app/controllers/tasks_controller.rb
@@ -0,0 +1,38 @@
+class TasksController < ApplicationController
+    def new
+        @task = Task.new
+        render :show_form
+    end
+
+    def create
+        @task = Task.create(task_params)
+        @tasks = Task.all
+
+        render :hide_form
+    end
+
+    def edit
+        @task = Task.find(params[:id])
+        render :show_form
+    end
+
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        @tasks = Task.all
+    end
+
+    def update
+        @task = Task.find(params[:id])
+        @task.update_attributes(task_params)
+        @tasks = Task.all
+
+        render :hide_form
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:title, :note, :completed)
+    end
+end
